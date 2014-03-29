1d8bd5be617410addc6684b64fe79889fdb4a457
cymrucoder <git.cmt@gmail.com>
2014-03-29 20:47:29 +0000

    Fixed bug in factory code.

--- EnvironmentBehFactory.cpp
+++ EnvironmentBehFactory.cpp
@@ -12,11 +12,11 @@ EnvironmentBehaviour* EnvironmentBehFactory::makeEnvironmentBehaviour(int enviro
     {
         return new EnvironmentAntSystem(edges, 0.7f);
     }
-    else if (environmentBehaviourType == ENVIRONMENT_BEH_TYPE_AS)
+    else if (environmentBehaviourType == ENVIRONMENT_BEH_TYPE_MMAS)
     {
         return new EnvironmentMaxMinAS(edges, 0.7f, 0.5f);// Min phero
     }
-    else if (environmentBehaviourType == ENVIRONMENT_BEH_TYPE_AS)
+    else if (environmentBehaviourType == ENVIRONMENT_BEH_TYPE_RBAS)
     {
         return new EnvironmentRankBasedAS(edges, 0.7f, 4, 4);// Ranked ants, elitist ants
     }
